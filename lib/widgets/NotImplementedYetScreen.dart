29faf9a5eba98d6f5085809f5b6538328bae7738
Tim R <tim.rothenpieler.extern@bdr.de>
2024-01-13 09:08:35 +0100

    initial ocmmit

--- lib/widgets/NotImplementedYetScreen.dart
+++ lib/widgets/NotImplementedYetScreen.dart
@@ -1,4 +1,4 @@
-import 'package:buzz/Widgets/comuntitle.dart';
+import 'package:buzz/widgets/comuntitle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
