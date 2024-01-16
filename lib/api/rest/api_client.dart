a63f91294f793f731bfcd0f7b7e1be88fb5d2aea
Tim R <tim.rothenpieler.extern@bdr.de>
2024-01-16 01:38:22 +0100

    Buttons are working

--- lib/api/rest/api_client.dart
+++ lib/api/rest/api_client.dart
@@ -21,8 +21,8 @@ class ApiClient {
 
   ApiClient(
       {
-        //this.basePath = "https://insuranceconstract-rest-backend.azurewebsites.net/api/v1/"
-      this.basePath = "http://localhost:8080/api/v1/"
+        this.basePath = "https://insuranceconstract-rest-backend.azurewebsites.net/api/v1/"
+      //this.basePath = "http://localhost:8080/api/v1/"
       });
 
   void addDefaultHeader(String key, String value) {
