05f41f0834f682080facae9f0cbff1bd260c9279
Tim R <tim.rothenpieler.extern@bdr.de>
2024-02-22 04:18:29 +0100

    jens optadata challenge

--- lib/newpages/comunwidget4.dart
+++ lib/newpages/comunwidget4.dart
@@ -4,24 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:provider/provider.dart';
 
-class ComunWidget4 extends StatefulWidget {
+class ComunWidget4 extends StatelessWidget {
   final double percentage;
   final bool big;
 
-  const ComunWidget4({Key? key, required this.percentage, this.big = true})
+  ComunWidget4({Key? key, required this.percentage, this.big = false})
       : super(key: key);
 
   @override
-  State<ComunWidget4> createState() => _ComunWidget4State();
-}
-
-class _ComunWidget4State extends State<ComunWidget4> {
-  @override
   Widget build(BuildContext context) {
     Color color = Colors.lightGreen;
-    if (widget.percentage < 0.3) {
+    if (percentage < 0.3) {
       color = Colors.redAccent;
-    } else if (widget.percentage < 0.6) {
+    } else if (percentage < 0.6) {
       color = Colors.orangeAccent;
     } else {
       color = Colors.lightGreen;
@@ -31,15 +26,15 @@ class _ComunWidget4State extends State<ComunWidget4> {
       builder: (context, value, child) => CircularPercentIndicator(
         curve: Curves.easeOut,
         animationDuration: 3000,
-        radius: widget.big ? 70 : 40.0,
-        lineWidth: widget.big ? 8 : 4.0,
+        radius: big ? 70 : 40.0,
+        lineWidth: big ? 8 : 4.0,
         animation: true,
-        percent: widget.percentage,
+        percent: percentage,
         animateFromLastPercent: true,
         center: Text(
-          "${(widget.percentage * 100).toInt()}%",
+          "${(percentage * 100).toInt()}%",
           style: TextStyle(
-              fontSize: widget.big ? 18 : 13.0,
+              fontSize: big ? 18 : 13.0,
               fontWeight: FontWeight.w600,
               color: notifire!.getMainText),
         ),
@@ -50,3 +45,39 @@ class _ComunWidget4State extends State<ComunWidget4> {
     );
   }
 }
+
+// class _ComunWidget4State extends State<ComunWidget4> {
+//   @override
+//   Widget build(BuildContext context) {
+//     Color color = Colors.lightGreen;
+//     if (widget.percentage < 0.3) {
+//       color = Colors.redAccent;
+//     } else if (widget.percentage < 0.6) {
+//       color = Colors.orangeAccent;
+//     } else {
+//       color = Colors.lightGreen;
+//     }
+//
+//     return Consumer<ColorNotifire>(
+//       builder: (context, value, child) => CircularPercentIndicator(
+//         curve: Curves.easeOut,
+//         animationDuration: 3000,
+//         radius: widget.big ? 70 : 40.0,
+//         lineWidth: widget.big ? 8 : 4.0,
+//         animation: true,
+//         percent: widget.percentage,
+//         animateFromLastPercent: true,
+//         center: Text(
+//           "${(widget.percentage * 100).toInt()}%",
+//           style: TextStyle(
+//               fontSize: widget.big ? 18 : 13.0,
+//               fontWeight: FontWeight.w600,
+//               color: notifire!.getMainText),
+//         ),
+//         backgroundColor: Colors.grey.shade900,
+//         circularStrokeCap: CircularStrokeCap.round,
+//         progressColor: color,
+//       ),
+//     );
+//   }
+// }
